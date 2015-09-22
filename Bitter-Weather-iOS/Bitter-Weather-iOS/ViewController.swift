b2ca3bde248b2cb3004807b26bb67c07d8f59993
Harnek Sidhu <hsidhu@architech.ca>
2015-09-21 20:28:53 -0400

    Xcode Project Generation

---
+++ Bitter-Weather-iOS/Bitter-Weather-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bitter-Weather-iOS
+//
+//  Created by Harnek Sidhu on 2015-09-21.
+//  Copyright © 2015 Bitter Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
